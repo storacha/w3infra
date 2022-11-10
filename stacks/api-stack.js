784d399e678ea4d23f0260fd7aab3bd6d8267707
Vasco Santos <santos.vasco10@gmail.com>
2022-11-10 12:56:49 +0100

    fix: rename api, add tsc and lint to ci

--- stacks/api-stack.js
+++ stacks/api-stack.js
@@ -4,7 +4,7 @@ import { Api } from '@serverless-stack/resources'
  * @param {import('@serverless-stack/resources').StackContext} properties
  */
 export function ApiStack({ stack }) {
-  const api = new Api(stack, 'api', {
+  const api = new Api(stack, 'http-gateway', {
     routes: {
       'GET /hello': 'functions/hello.handler',
     },
