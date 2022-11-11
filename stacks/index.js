54f48889330902bb3db14cfe25ce244a6e0010ca
Vasco Santos <santos.vasco10@gmail.com>
2022-11-11 11:15:56 +0000

    chore: rename to upload api (#5)

--- stacks/index.js
+++ stacks/index.js
@@ -17,8 +17,8 @@ export default function (app) {
 
   // tags let us discover all the aws resource costs incurred by this app
   // see: https://docs.sst.dev/advanced/tagging-resources
-  Tags.of(app).add('Project', 'w3up-api')
-  Tags.of(app).add('Repository', 'https://github.com/web3-storage/w3up-api')
+  Tags.of(app).add('Project', 'upload-api')
+  Tags.of(app).add('Repository', 'https://github.com/web3-storage/upload-api')
   Tags.of(app).add('Environment', `${app.stage}`)
   Tags.of(app).add('ManagedBy', 'SST')
 }
