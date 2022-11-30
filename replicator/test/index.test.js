2f915e5ae915de43c73be714a0ecee32cc2c203d
Vasco Santos <santos.vasco10@gmail.com>
2022-11-30 14:55:17 +0100

    feat: replicator stack

---
+++ replicator/test/index.test.js
@@ -0,0 +1,137 @@
+import { test } from './helpers/context.js'
+
+import {
+  GetObjectCommand,
+  PutObjectCommand,
+} from '@aws-sdk/client-s3'
+import { encode } from 'multiformats/block'
+import { identity } from 'multiformats/hashes/identity'
+import { sha256 as hasher } from 'multiformats/hashes/sha2'
+import * as pb from '@ipld/dag-pb'
+import { CarBufferWriter, CarWriter } from '@ipld/car'
+import { toString } from 'uint8arrays'
+
+import { getSideIndex } from '../../satnav/index.js'
+import { createS3, createBucket } from './helpers/resources.js'
+import { replicate } from '../index.js'
+
+test.before(async t => {
+  const { client } = await createS3({ port: 9000 })
+
+  t.context.s3Client = client
+})
+
+test('copy CARs from origin bucket to replicator bucket', async t => {
+  const originBucketName = await createBucket(t.context.s3Client)
+  const destinationBucketName = await createBucket(t.context.s3Client)
+
+  // Write original car to origin bucket
+  const id = await encode({
+    value: pb.prepare({ Data: 'a red car on the street!' }),
+    codec: pb,
+    hasher: identity,
+  })
+  const parent = await encode({
+    value: pb.prepare({ Links: [id.cid] }),
+    codec: pb,
+    hasher,
+  })
+  const car = CarBufferWriter.createWriter(Buffer.alloc(1000), {
+    roots: [parent.cid],
+  })
+  car.write(parent)
+
+  const Body = car.close()
+  const digest = await hasher.digest(Body)
+  const checksum = toString(digest.digest, 'base64pad')
+
+  const key = `${parent.cid.toString()}/${parent.cid.toString()}`
+  await t.context.s3Client.send(
+    new PutObjectCommand({
+      Bucket: originBucketName,
+      Key: key,
+      Body,
+      ChecksumSHA256: checksum,
+    })
+  )
+
+  const record = {
+    bucketName: originBucketName,
+    bucketRegion: 'us-west-2',
+    key,
+  }
+
+  await replicate({
+    record,
+    destinationBucket: t.context.s3Client,
+    originBucket: t.context.s3Client,
+    destinationBucketName
+  })
+
+  // Check if written files exist
+  const copiedCarResponse = await t.context.s3Client.send(
+    new GetObjectCommand({
+      Bucket: destinationBucketName,
+      Key: key,
+    })
+  )
+  t.is(copiedCarResponse.$metadata.httpStatusCode, 200)
+})
+
+test('copy satnav index to replicator bucket', async t => {
+  const originBucketName = await createBucket(t.context.s3Client)
+  const destinationBucketName = await createBucket(t.context.s3Client)
+
+  const id = await encode({
+    value: pb.prepare({ Data: 'a red car on the street!' }),
+    codec: pb,
+    hasher: identity,
+  })
+
+  const parent = await encode({
+    value: pb.prepare({ Links: [id.cid] }),
+    codec: pb,
+    hasher,
+  })
+
+  const { writer, out } = await CarWriter.create([parent.cid])
+  writer.put(parent)
+  const [sideIndex, ] = await Promise.all([
+    getSideIndex(out),
+    writer.close()
+  ])
+
+  const key = `${parent.cid.toString()}/${parent.cid.toString()}.idx`
+  await t.context.s3Client.send(
+    new PutObjectCommand({
+      Bucket: originBucketName,
+      Key: key,
+      Body: sideIndex,
+      Metadata: {
+        first: 'true'
+      }
+    })
+  )
+
+  const record = {
+    bucketName: originBucketName,
+    bucketRegion: 'us-west-2',
+    key,
+  }
+
+  await replicate({
+    record,
+    destinationBucket: t.context.s3Client,
+    originBucket: t.context.s3Client,
+    destinationBucketName
+  })
+
+  // Check if written files exist
+  const copiedCarResponse = await t.context.s3Client.send(
+    new GetObjectCommand({
+      Bucket: destinationBucketName,
+      Key: key,
+    })
+  )
+  t.is(copiedCarResponse.$metadata.httpStatusCode, 200)
+})
---
+++ stacks/replicator-stack.js
@@ -0,0 +1,142 @@
+import {
+  Function,
+  Queue,
+  use
+} from '@serverless-stack/resources'
+import { Duration, aws_events as awsEvents } from 'aws-cdk-lib'
+import { BusStack } from './bus-stack.js'
+
+import { CARPARK_EVENT_BRIDGE_SOURCE_EVENT } from '../carpark/event-bus/source.js'
+import { SATNAV_EVENT_BRIDGE_SOURCE_EVENT } from '../satnav/event-bus/source.js'
+
+/**
+ * @param {import('@serverless-stack/resources').StackContext} properties
+ */
+export function ReplicatorStack({ stack }) {
+  stack.setDefaultFunctionProps({
+    srcPath: 'replicator'
+  })
+
+  // Get Event Bus reference
+  const { eventBus } = use(BusStack)
+
+  // CAR replicator lambda
+  const carparkReplicatorHandler = new Function(
+    stack,
+    'carpark-replicator-handler',
+    {
+      environment: {
+        REPLICATOR_ACCOUNT_ID: process.env.REPLICATOR_ACCOUNT_ID || '',
+        REPLICATOR_ACCESS_KEY_ID: process.env.REPLICATOR_ACCESS_KEY_ID || '',
+        REPLICATOR_SECRET_ACCESS_KEY:
+          process.env.REPLICATOR_SECRET_ACCESS_KEY || '',
+        REPLICATOR_BUCKET_NAME: process.env.REPLICATOR_CAR_BUCKET_NAME || '',
+      },
+      permissions: ['s3:*'],
+      handler: 'functions/replicator.handler',
+      timeout: 15 * 60,
+    }
+  )
+
+  // Satnav replicator lambda
+  const satnavReplicatorHandler = new Function(
+    stack,
+    'satnav-replicator-handler',
+    {
+      environment: {
+        REPLICATOR_ACCOUNT_ID: process.env.REPLICATOR_ACCOUNT_ID || '',
+        REPLICATOR_ACCESS_KEY_ID: process.env.REPLICATOR_ACCESS_KEY_ID || '',
+        REPLICATOR_SECRET_ACCESS_KEY:
+          process.env.REPLICATOR_SECRET_ACCESS_KEY || '',
+        REPLICATOR_BUCKET_NAME: process.env.REPLICATOR_INDEX_BUCKET_NAME || '',
+      },
+      permissions: ['s3:*'],
+      handler: 'functions/replicator.handler',
+      timeout: 15 * 60,
+    }
+  )
+
+  // Queues
+  const carReplicatorQueue = new Queue(stack, 'car-replicator-queue', {
+    consumer: {
+      function: carparkReplicatorHandler,
+      cdk: {
+        eventSource: {
+          batchSize: 1,
+        },
+      }
+    },
+    cdk: {
+      queue: {
+        // Needs to be set as less or equal than consumer function
+        visibilityTimeout: Duration.seconds(15 * 60),
+      },
+    },
+  })
+
+  const satnavReplicatorQueue = new Queue(stack, 'satnav-replicator-queue', {
+    consumer: {
+      function: satnavReplicatorHandler,
+      cdk: {
+        eventSource: {
+          batchSize: 1,
+        },
+      }
+    },
+    cdk: {
+      queue: {
+        // Needs to be set as less or equal than consumer function
+        visibilityTimeout: Duration.seconds(15 * 60),
+      },
+    },
+  })
+
+  // Event Bus Targets
+  const targetMessage = awsEvents.RuleTargetInput.fromObject({
+    bucketRegion: awsEvents.EventField.fromPath('$.detail.region'),
+    bucketName: awsEvents.EventField.fromPath('$.detail.bucketName'),
+    key: awsEvents.EventField.fromPath('$.detail.key')
+  })
+
+  /** @type {import('@serverless-stack/resources').EventBusQueueTargetProps} */
+  const carTargetReplicatorQueue = {
+    type: 'queue',
+    queue: carReplicatorQueue,
+    cdk: {
+      target: {
+        message: targetMessage,
+      },
+    }
+  }
+
+  /** @type {import('@serverless-stack/resources').EventBusQueueTargetProps} */
+  const satnavTargetReplicatorQueue = {
+    type: 'queue',
+    queue: satnavReplicatorQueue,
+    cdk: {
+      target: {
+        message: targetMessage,
+      },
+    }
+  }
+
+  // Replicate CARPARK and SATNAV write events
+  eventBus.addRules(stack, {
+    newCarToReplicate: {
+      pattern: {
+        source: [CARPARK_EVENT_BRIDGE_SOURCE_EVENT],
+      },
+      targets: {
+        carTargetReplicatorQueue
+      }
+    },
+    newSatnavIndexToReplicate: {
+      pattern: {
+        source: [SATNAV_EVENT_BRIDGE_SOURCE_EVENT],
+      },
+      targets: {
+        satnavTargetReplicatorQueue
+      }
+    }
+  })
+}
