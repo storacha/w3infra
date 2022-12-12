0d55985ea1a36176f742a3bb83ac402d9dee82a4
Vasco Santos <santos.vasco10@gmail.com>
2022-12-12 14:22:29 +0100

    chore: improve namings for bus event

--- ucan-invocation/event-bus/source.js
+++ ucan-invocation/event-bus/source.js
@@ -10,7 +10,7 @@ export const UCAN_STORE_EVENT_BRIDGE_SOURCE_EVENT = 'ucan_store_bucket'
  * @param {string} eventBusName
  */
  export async function notifyBus(event, eventBridge, eventBusName) {
-  const entries = event.Records
+  const s3Events = event.Records
     ? event.Records.map((r) => ({
         key: r?.s3?.object?.key,
         region: r?.awsRegion || 'us-west-2',
@@ -19,12 +19,12 @@ export const UCAN_STORE_EVENT_BRIDGE_SOURCE_EVENT = 'ucan_store_bucket'
       })).filter((entry) => entry.key && entry.key.endsWith('.car'))
     : []
 
-  if (entries.length > 0) {
-    const feedbackEntries = entries.map((entry) => ({
+  if (s3Events.length > 0) {
+    const feedbackEntries = s3Events.map((s3Event) => ({
       EventBusName: eventBusName,
       Source: UCAN_STORE_EVENT_BRIDGE_SOURCE_EVENT,
       DetailType: 'ucan_car_added',
-      Detail: JSON.stringify(entry),
+      Detail: JSON.stringify(s3Event),
     }))
     await eventBridge.putEvents({ Entries: feedbackEntries })
   }
