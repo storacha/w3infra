6140f85037502e7c4fbc1057541197b4da0e8f04
Vasco Santos <santos.vasco10@gmail.com>
2022-12-13 09:41:11 +0000

    feat: persist ucan invocation car to s3 and replicate to r2 (#83)
    Per last night call, persisting CAR files of UCAN invocations handled by
    the service into S3 with replication to R2.
    
    Implementation details:
    - An abstraction store for the bucket was created with same pattern as
    we use for Dudewhere + Carpark
    - persistence happens when ucan route handler was successfully executed
    - create ucan-invocation stack
    - it includes S3 UCAN Bucket and its function to send notifications of
    written files to EventBus, so that Replicator can kick in
    - future: will include the Kinesis stream, as well as its consumer
    functions
    - replicator stack updated with tracking events for UCAN bucket and
    replicating them into R2 bucket with new bucket
    
    TODO:
    - [x] create R2 buckets
    - [x] set env variables in seed.run

--- upload-api/test/helpers/context.js
+++ upload-api/test/helpers/context.js
@@ -11,12 +11,20 @@ import anyTest from 'ava'
  * @property {string} accessServiceDID
  * @property {string} accessServiceURL
  * 
+ * @typedef {object} UcanInvocationContext
+ * @property {import('@aws-sdk/client-s3').S3Client} s3Client
+ * @property {import('@aws-sdk/client-s3').ServiceInputTypes} s3ClientOpts
+ * 
  * @typedef {import("ava").TestFn<Awaited<UcantoServerContext>>} TestStoreFn
+ * @typedef {import("ava").TestFn<Awaited<UcanInvocationContext>>} TestUcanInvocationFn
  * @typedef {import("ava").TestFn<Awaited<any>>} TestAnyFn
  */
 
 // eslint-disable-next-line unicorn/prefer-export-from
 export const testStore = /** @type {TestStoreFn} */ (anyTest)
+
+// eslint-disable-next-line unicorn/prefer-export-from
+export const testUcanInvocation = /** @type {TestUcanInvocationFn} */ (anyTest)
 
 // eslint-disable-next-line unicorn/prefer-export-from
 export const test  = /** @type {TestAnyFn} */ (anyTest)
