7b9cb41f88723a1aca659ab262fb8d4fd46e6b9a
Benjamin Goering <171782+gobengo@users.noreply.github.com>
2022-12-15 14:39:53 -0800

    feat(upload-api): versionGet endpoint has publicKey prop which is always a did:key (#112)
    * also add to homeGet
    
    Motivation:
    * https://github.com/web3-storage/w3infra/issues/105

--- upload-api/config.js
+++ upload-api/config.js
@@ -7,11 +7,13 @@ import * as DID from '@ipld/dag-ucan/did'
  * @param {object} config
  * @param {string} config.PRIVATE_KEY - multiformats private key of primary signing key
  * @param {string} [config.UPLOAD_API_DID] - public DID for the upload service (did:key:... derived from PRIVATE_KEY if not set)
+ * @returns {import('@ucanto/principal/ed25519').Signer.Signer}
  */
  export function getServiceSigner(config) {
   const signer = ed25519.parse(config.PRIVATE_KEY)
   if (config.UPLOAD_API_DID) {
-    return signer.withDID(DID.parse(config.UPLOAD_API_DID).did())
+    const did = DID.parse(config.UPLOAD_API_DID).did()
+    return signer.withDID(did)
   }
   return signer
 }
