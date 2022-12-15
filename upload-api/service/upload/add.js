216b0690a1dd58e3c03c2d896148627bb5f7a61d
Vasco Santos <santos.vasco10@gmail.com>
2022-12-15 15:18:23 +0000

    fix: verify space registered on upload add

--- upload-api/service/upload/add.js
+++ upload-api/service/upload/add.js
@@ -17,11 +17,13 @@ export function uploadAddProvider(context) {
     Upload.add,
     async ({ capability, invocation }) => {
       const { root, shards } = capability.nb
-
-      // Only use capability account for now to check if account is registered.
-      // This must change to access account/info!!
-      // We need to use https://github.com/web3-storage/w3protocol/blob/9d4b5bec1f0e870233b071ecb1c7a1e09189624b/packages/access/src/agent.js#L270
       const space = Server.DID.parse(capability.with).did()
+      const issuer = invocation.issuer.did()
+      const isVerified = await context.access.verifyInvocation(invocation)
+
+      if (!isVerified) {
+        return new Server.Failure(`${issuer} is not delegated capability ${Upload.add.can} on ${space}`)
+      }
 
       const [res] = await Promise.all([
         // Store in Database
