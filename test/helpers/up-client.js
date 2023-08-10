bb345dbb48ddedf0e5e2148a3c4202b5ba0e398a
Travis Vachon <travis.vachon@gmail.com>
2023-08-10 15:02:30 -0700

    Revert "chore: more timeout bumping in hopes of getting integration tests working"
    This reverts commit 37f2a4e17a58290960d06626dbce2ca285501e1a.

--- test/helpers/up-client.js
+++ test/helpers/up-client.js
@@ -47,7 +47,7 @@ export async function setupNewClient (uploadServiceUrl, options = {}) {
   const { mailslurp, id: inboxId, email } = await createMailSlurpInbox()
   const client = await createNewClient(uploadServiceUrl)
 
-  const timeoutMs = process.env.MAILSLURP_TIMEOUT ? parseInt(process.env.MAILSLURP_TIMEOUT) : 120_000
+  const timeoutMs = process.env.MAILSLURP_TIMEOUT ? parseInt(process.env.MAILSLURP_TIMEOUT) : 60_000
   const authorizePromise = client.authorize(email)
   // click link in email
   const latestEmail = await mailslurp.waitForLatestEmail(inboxId, timeoutMs)
