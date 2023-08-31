4d2a03d470565de4e332270ce098bc496d03c2cf
Travis Vachon <travis.vachon@gmail.com>
2023-08-30 21:11:58 -0700

    fix: one more typo

--- upload-api/tables/space-metrics.js
+++ upload-api/tables/space-metrics.js
@@ -3,7 +3,7 @@ import {
   GetItemCommand,
 } from '@aws-sdk/client-dynamodb'
 import { marshall, unmarshall } from '@aws-sdk/util-dynamodb'
-import { METRICS_NAMES } from '../../ucan-invocation/constants'
+import { METRICS_NAMES } from '../../ucan-invocation/constants.js'
 
 
 /**
