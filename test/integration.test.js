16a9099dc8afba257897562c98a1f0d179ae168b
Vasco Santos <santos.vasco10@gmail.com>
2023-09-19 16:04:52 +0200

    chore: run full interop tests suite

--- test/integration.test.js
+++ test/integration.test.js
@@ -114,7 +114,7 @@ test('authorizations can be blocked by email or domain', async t => {
 })
 
 // Integration test for all flow from uploading a file to Kinesis events consumers and replicator
-test.skip('w3infra integration flow', async t => {
+test('w3infra integration flow', async t => {
   const client = await setupNewClient(t.context.apiEndpoint)
   const spaceDid = client.currentSpace()?.did()
   if (!spaceDid) {
