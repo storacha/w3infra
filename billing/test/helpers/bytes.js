8ef9d894593d6cbb6acad0e151103f63f550f06a
Alan Shaw <alan.shaw@protocol.ai>
2023-10-25 17:36:36 +0100

    fix: use node webcrypto

--- billing/test/helpers/bytes.js
+++ billing/test/helpers/bytes.js
@@ -1,9 +1,11 @@
+import { webcrypto } from 'node:crypto'
+
 /** @param {number} size */
 export const randomBytes = size => {
   const bytes = new Uint8Array(size)
   while (size) {
     const chunk = new Uint8Array(Math.min(size, 65_536))
-    crypto.getRandomValues(chunk)
+    webcrypto.getRandomValues(chunk)
 
     size -= bytes.length
     bytes.set(chunk, size)
