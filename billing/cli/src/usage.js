de6e19a2a1744e27cf748fbf7bf28cc5c43ecb26
Alan Shaw <alan.shaw@protocol.ai>
2023-10-27 16:21:55 +0100

    feat: add usage cmd

--- billing/cli/src/usage.js
+++ billing/cli/src/usage.js
@@ -1,41 +1,54 @@
-
-import { QueryCommand } from '@aws-sdk/client-dynamodb'
+import Big from 'big.js'
 import { asDIDMailto } from '../../data/lib.js'
 import { mustGetEnv } from '../../functions/lib.js'
-import { createStoreGetterClient, createStoreListerClient } from '../../tables/client.js'
+import { createStoreListerClient } from '../../tables/client.js'
 import { getDynamo } from './lib.js'
-import { encodeKey, decode } from '../../data/usage.js'
+import { decode, lister } from '../../data/usage.js'
+
+const GB = 1024 * 1024 * 1024
+const perGBMonth = 10
 
 /**
- * @param {object} config
- * @param {string} config.customer
- * @param {string} config.datetime
+ * @param {string} customerParam
+ * @param {string} fromParam
  */
-export const usage = async (config) => {
-  const customer = asDIDMailto(config.customer)
-  const from = new Date(config.datetime)
+export const usage = async (customerParam, fromParam) => {
+  const customer = asDIDMailto(customerParam)
+  const from = new Date(fromParam)
   if (isNaN(from.getTime())) {
     console.error('invalid ISO date')
     process.exit(1)
   }
 
   const tableName = mustGetEnv('USAGE_TABLE_NAME')
-
   const dynamo = getDynamo()
 
-  const cmd = new QueryCommand({
-    TableName: tableName,
-    KeyConditions: {
-      customer: {
-        ComparisonOperator: 'EQ',
-        AttributeValueList: [{ S: customer }]
-      },
-      from: {
-        ComparisonOperator: 'GE',
-        AttributeValueList: [{ S: from.toISOString() }]
-      }
-    }
+  const store = createStoreListerClient(dynamo, {
+    tableName,
+    encodeKey: lister.encodeKey,
+    decode
   })
 
-  await dynamo.send(cmd)
+  const { ok: listing, error } = await store.list({ customer, from })
+  if (error) {
+    console.error(error.message)
+    process.exit(1)
+  }
+
+  console.log(`Customer: ${customer}`)
+  console.log('Usage:')
+
+  let total = 0
+  if (listing.results.length) {
+    for (const usage of listing.results) {
+      const duration = usage.to.getTime() - usage.from.getTime()
+      const cost = new Big(usage.toString()).div(duration).div(GB).mul(perGBMonth).toNumber()
+      total += cost
+      console.log(`  ${usage.provider} ${usage.space} ${usage.usage} $${total}`)
+    }
+  } else {
+    console.log('  No usage in period')
+  }
+  console.log('Total: $0')
+
 }
