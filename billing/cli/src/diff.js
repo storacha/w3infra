94b023486f7976121d876f50f20dc1086b493582
Alan Shaw <alan.shaw@protocol.ai>
2023-11-01 12:08:46 +0000

    refactor: tidyup

--- billing/cli/src/diff.js
+++ billing/cli/src/diff.js
@@ -1,15 +1,15 @@
-import { DynamoDBClient, QueryCommand } from '@aws-sdk/client-dynamodb'
-import { createStorePutterClient, createStoreGetterClient } from '../../tables/client.js'
-import * as SpaceDiff from '../../data/space-diff.js'
+import { QueryCommand } from '@aws-sdk/client-dynamodb'
 import * as Consumer from '../../data/consumer.js'
-import * as Subscription from '../../data/subscription.js'
 import { randomLink } from '../../test/helpers/dag.js'
-import { mustGetEnv } from '../../functions/lib.js'
+import { expect, mustGetEnv } from '../../functions/lib.js'
 import { marshall, unmarshall } from '@aws-sdk/util-dynamodb'
 import Bytes from 'bytes'
+import { createSubscriptionStore } from '../../tables/subscription.js'
+import { createSpaceDiffStore } from '../../tables/space-diff.js'
+import { getDynamo, isValidDate } from './lib.js'
 
 /**
- * Remove some bytes to the space at the passed ISO timestamp.
+ * Remove some bytes from the space at the passed ISO timestamp.
  * 
  * $ billing diff remove did:key:space0 3MB 2023-09-16T09:00:00.000Z
  * 
@@ -40,25 +40,25 @@ export async function diffAdd (space, rawBytes, datetime) {
  * Insert a diff record
  * 
  * @param {string} space
- * @param {number} change - positive or negative integer in bytes
+ * @param {number} delta - positive or negative integer in bytes
  * @param {string} datetime
  */
-async function insertDiff (space, change, datetime) {
+async function insertDiff (space, delta, datetime) {
   const receiptAt = new Date(datetime)
-  if (!isValidDate) {
+  if (!isValidDate(receiptAt)) {
     throw new Error(`${datetime} is not a valid date-time`)
   }
-  const region = process.env.AWS_REGION
-  const dynamo = new DynamoDBClient({ region })
+  const dynamo = getDynamo()
   const consumer = await getConsumerBySpace(dynamo, space)
-  const subscriptionStore = createStoreGetterClient( dynamo, {
-    tableName: mustGetEnv('SUBSCRIPTION_TABLE_NAME'),
-    ...Subscription
+  const subscriptionStore = createSubscriptionStore(dynamo, {
+    tableName: mustGetEnv('SUBSCRIPTION_TABLE_NAME')
   })
-  const subscription = await okOrThrow(subscriptionStore, consumer)
-  const spaceDiffStore = createStorePutterClient(dynamo, {
-    tableName: mustGetEnv('SPACE_DIFF_TABLE_NAME'),
-    ...SpaceDiff
+  const subscription = expect(
+    await subscriptionStore.get({ provider: consumer.provider, subscription: consumer.subscription }),
+    `getting subscription: ${consumer.subscription}`
+  )
+  const spaceDiffStore = createSpaceDiffStore(dynamo, {
+    tableName: mustGetEnv('SPACE_DIFF_TABLE_NAME')
   })
   const res = await spaceDiffStore.put({
     cause: randomLink(),
@@ -66,24 +66,18 @@ async function insertDiff (space, change, datetime) {
     space: consumer.consumer,
     subscription: subscription.subscription,
     insertedAt: new Date(),
-    provider: 'did:web:web3.storage',
-    change,
+    provider: consumer.provider,
+    delta,
     receiptAt
   })
   console.log(res)
-
-}
-
-/** @param {Date} d */
-function isValidDate (d) {
-  return !isNaN(d.getTime())
 }
 
 /**
- * @param {DynamoDBClient} dynamo
+ * @param {import('@aws-sdk/client-dynamodb').DynamoDBClient} dynamo
  * @param {string} space
  */
-export async function getConsumerBySpace (dynamo, space) {
+async function getConsumerBySpace (dynamo, space) {
   const res = await dynamo.send(new QueryCommand({
     TableName: mustGetEnv('CONSUMER_TABLE_NAME'),
     IndexName: 'consumer',
@@ -95,24 +89,9 @@ export async function getConsumerBySpace (dynamo, space) {
   if (!res.Items || res.Items.length === 0) {
     throw new Error(`Failed to get consumer for ${space}`)
   }
-  // const decoded = Consumer.decode(unmarshall(res.Items[0]))
-  // if (decoded.error) {
-  //   throw new Error(decoded.error.message)
-  // }
-  // return decoded.ok
-  return unmarshall(res.Items[0])
-}
-
-/**
- * @template {object} K
- * @template V
- * @param {import('../../lib/api.js').StoreGetter<K,V>} storeGetter
- * @param {K} key
- */
-async function okOrThrow (storeGetter, key) {
-  const res = await storeGetter.get(key)
-  if (res.error) {
-    throw new Error(res.error.message)
+  const decoded = Consumer.lister.decode(unmarshall(res.Items[0]))
+  if (decoded.error) {
+    throw new Error(decoded.error.message)
   }
-  return res.ok
+  return decoded.ok
 }
--- billing/cli/src/space.js
+++ billing/cli/src/space.js
@@ -1,17 +1,18 @@
 import { Signer } from '@ucanto/principal/ed25519'
-import { asDIDMailto } from '../../data/lib.js'
-import { mustGetEnv } from '../../functions/lib.js'
+import { Schema } from '../../data/lib.js'
+import { expect, mustGetEnv } from '../../functions/lib.js'
 import { getDynamo } from './lib.js'
 import { randomLink } from '../../test/helpers/dag.js'
 import { PutItemCommand } from '@aws-sdk/client-dynamodb'
 import { marshall } from '@aws-sdk/util-dynamodb'
 import { startOfLastMonth } from '../../lib/util.js'
+import { createSpaceSnapshotStore } from '../../tables/space-snapshot.js'
 
 /**
  * @param {string} customerParam
  */
 export const spaceAdd = async (customerParam) => {
-  const customer = asDIDMailto(customerParam)
+  const customer = Schema.did({ method: 'mailto' }).from(customerParam)
 
   const signer = await Signer.generate()
   const space = signer.did()
@@ -22,6 +23,7 @@ export const spaceAdd = async (customerParam) => {
   const subscriptionTableName = mustGetEnv('SUBSCRIPTION_TABLE_NAME')
   const spaceSnapshotTableName = mustGetEnv('SPACE_SNAPSHOT_TABLE_NAME')
   const dynamo = getDynamo()
+  const now = new Date()
 
   await dynamo.send(new PutItemCommand({
     TableName: consumerTableName,
@@ -30,8 +32,8 @@ export const spaceAdd = async (customerParam) => {
       provider,
       subscription,
       cause: randomLink().toString(),
-      insertedAt: new Date().toISOString(),
-      updatedAt: new Date().toISOString()
+      insertedAt: now.toISOString(),
+      updatedAt: now.toISOString()
     })
   }))
 
@@ -42,10 +44,25 @@ export const spaceAdd = async (customerParam) => {
       provider,
       subscription,
       cause: randomLink().toString(),
-      insertedAt: new Date().toISOString(),
-      updatedAt: new Date().toISOString()
+      insertedAt: now.toISOString(),
+      updatedAt: now.toISOString()
     })
   }))
+
+  const spaceSnapshotStore = createSpaceSnapshotStore(dynamo, {
+    tableName: spaceSnapshotTableName
+  })
+
+  expect(
+    await spaceSnapshotStore.put({
+      provider,
+      space,
+      size: 0n,
+      recordedAt: startOfLastMonth(now),
+      insertedAt: now
+    }),
+    'putting space snapshot'
+  )
 
   await dynamo.send(new PutItemCommand({
     TableName: spaceSnapshotTableName,
@@ -54,10 +71,10 @@ export const spaceAdd = async (customerParam) => {
       space,
       provider,
       size: 0,
-      recordedAt: startOfLastMonth().toISOString(),
-      insertedAt: new Date().toISOString()
+      recordedAt: startOfLastMonth(now).toISOString(),
+      insertedAt: now.toISOString()
     })
   }))
 
-  console.log(`Space: ${space}`)
+  console.log({ ok: { space } })
 }
