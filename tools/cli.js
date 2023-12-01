07cc863d03ad90604642298de68921528ba6ed39
Vasco Santos <santos.vasco10@gmail.com>
2023-12-01 11:06:50 +0100

    feat: tool for following filecoin receipt chain

--- tools/cli.js
+++ tools/cli.js
@@ -3,6 +3,7 @@
 import sade from 'sade'
 
 import { fetchMetricsForSpaceCmd } from './fetch-metrics-for-space.js'
+import { followFilecoinReceiptChain } from './follow-filecoin-receipt-chain.js'
 import { migrateFromD1ToDynamo } from './d1-migration/add-to-dynamo.js'
 import { printD1ProvisionsEmails } from './d1-migration/print-d1-emails.js'
 import { verifyD1DynamoMigration } from './d1-migration/verify-d1-dynamo-migration.js'
@@ -14,6 +15,10 @@ cli.version('1.0.0')
 cli
   .command('fetch-metrics-for-space', 'Fetch metrics for a given space')
   .action(fetchMetricsForSpaceCmd)
+
+cli
+  .command('follow-filecoin-receipt-chain', 'Follow filecoin receipt chain for a piece')
+  .action(followFilecoinReceiptChain)
 
 cli
   .command('d1-dynamo-migration', 'Run the D1 -> Dynamo migration')
