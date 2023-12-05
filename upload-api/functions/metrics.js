07bd219642f881c0d2e6b018c16ad809b849f125
Vasco Santos <santos.vasco10@gmail.com>
2023-12-05 22:35:48 +0100

    feat: update metrics endpoint with filecoin basic metrics

--- upload-api/functions/metrics.js
+++ upload-api/functions/metrics.js
@@ -6,7 +6,9 @@ import {
   STORE_ADD,
   STORE_REMOVE,
   UPLOAD_ADD,
-  UPLOAD_REMOVE
+  UPLOAD_REMOVE,
+  AGGREGATE_OFFER,
+  AGGREGATE_ACCEPT
 } from '@web3-storage/w3infra-ucan-invocation/constants.js'
 
 import { createMetricsTable } from '../tables/metrics.js'
@@ -71,12 +73,22 @@ export async function recordMetrics (metrics, metricsTable) {
   metrics.invocations.inc({ 'can': STORE_REMOVE }, fetchedMetrics[METRICS_NAMES.STORE_REMOVE_TOTAL] || 0)
   metrics.invocations.inc({ 'can': UPLOAD_ADD }, fetchedMetrics[METRICS_NAMES.UPLOAD_ADD_TOTAL] || 0)
   metrics.invocations.inc({ 'can': UPLOAD_REMOVE }, fetchedMetrics[METRICS_NAMES.UPLOAD_REMOVE_TOTAL] || 0)
+
+  // aggregates count
+  metrics.aggregates.inc({ 'can': AGGREGATE_OFFER }, fetchedMetrics[METRICS_NAMES.AGGREGATE_OFFER_TOTAL] || 0)
+  metrics.aggregates.inc({ 'can': AGGREGATE_ACCEPT }, fetchedMetrics[METRICS_NAMES.AGGREGATE_ACCEPT_TOTAL] || 0)
+
+  metrics.aggregatedPieces.inc({ 'can': AGGREGATE_OFFER }, fetchedMetrics[METRICS_NAMES.AGGREGATE_OFFER_PIECES_TOTAL] || 0)
+  metrics.aggregatedPiecesBytes.inc({ 'can': AGGREGATE_OFFER }, fetchedMetrics[METRICS_NAMES.AGGREGATE_OFFER_PIECES_SIZE_TOTAL] || 0)
 }
 
 /**
  * @typedef {object} Metrics
  * @property {Prom.Counter<'can'>} bytes
  * @property {Prom.Counter<'can'>} invocations
+ * @property {Prom.Counter<'can'>} aggregates
+ * @property {Prom.Counter<'can'>} aggregatedPieces
+ * @property {Prom.Counter<'can'>} aggregatedPiecesBytes
  */
 
 function createRegistry (ns = 'w3up') {
@@ -93,6 +105,24 @@ function createRegistry (ns = 'w3up') {
       invocations: new Prom.Counter({
         name: `${ns}_invocations_total`,
         help: 'Total number of invocations.',
+        labelNames: ['can'],
+        registers: [registry]
+      }),
+      aggregates: new Prom.Counter({
+        name: `${ns}_aggregates_total`,
+        help: 'Total number of aggregates.',
+        labelNames: ['can'],
+        registers: [registry]
+      }),
+      aggregatedPieces: new Prom.Counter({
+        name: `${ns}_aggregated_pieces_total`,
+        help: 'Total number of pieces aggregated.',
+        labelNames: ['can'],
+        registers: [registry]
+      }),
+      aggregatedPiecesBytes: new Prom.Counter({
+        name: `${ns}_aggregated_pieces_bytes`,
+        help: 'Total bytes of pieces aggregated.',
         labelNames: ['can'],
         registers: [registry]
       }),
