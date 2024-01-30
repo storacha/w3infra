7e520f1adaceda61454a41b959181f3897e767f6
Travis Vachon <travis.vachon@gmail.com>
2024-01-30 11:10:58 -0800

    Revert "fix: use existing Stripe test key env var"
    This reverts commit d8ad82d587c715a9b1a54893b257fc366b95e16f.

--- upload-api/test/billing.test.js
+++ upload-api/test/billing.test.js
@@ -49,7 +49,7 @@ async function setupCustomer(stripe, email) {
 }
 
 test('stripe plan can be updated', async (t) => {
-  const stripeSecretKey = process.env.TESTING_STRIPE_TEST_PK
+  const stripeSecretKey = process.env.STRIPE_TEST_SECRET_KEY
   if (stripeSecretKey) {
     const stripe = new Stripe(stripeSecretKey, { apiVersion: '2023-10-16' })
     const billingProvider = createStripeBillingProvider(stripe)
@@ -88,6 +88,6 @@ test('stripe plan can be updated', async (t) => {
       }
     }
   } else {
-    t.fail('TESTING_STRIPE_TEST_PK environment variable is not set')
+    t.fail('STRIPE_TEST_SECRET_KEY environment variable is not set')
   }
 })
