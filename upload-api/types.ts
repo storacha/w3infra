832aa35bd8b0263122b54765ef60261e38d60657
Irakli Gozalishvili <contact@gozala.io>
2024-05-16 17:26:49 -0700

    feat: implement agent store

--- upload-api/types.ts
+++ upload-api/types.ts
@@ -1,9 +1,39 @@
 import * as UCAN from '@ipld/dag-ucan'
-import { DID, Link, Delegation, Signature, Block, UCANLink, ByteView, DIDKey, Result, Failure, Unit } from '@ucanto/interface'
+import {
+  DID,
+  Link,
+  Delegation,
+  Signature,
+  Block,
+  UCANLink,
+  ByteView,
+  DIDKey,
+  Result,
+  Failure,
+  Unit,
+} from '@ucanto/interface'
 import { UnknownLink } from 'multiformats'
 import { CID } from 'multiformats/cid'
 import { Kinesis } from '@aws-sdk/client-kinesis'
-import { AccountDID, ProviderDID, Service, SpaceDID, CarStoreBucket } from '@web3-storage/upload-api'
+import {
+  AccountDID,
+  ProviderDID,
+  Service,
+  SpaceDID,
+  CarStoreBucket,
+} from '@web3-storage/upload-api'
+
+export type {
+  UnknownLink,
+  AgentStore,
+  AgentMessage,
+  UCANLink,
+  Capability,
+  Result,
+  WriteError,
+  Writer,
+  Unit,
+} from '@web3-storage/upload-api'
 
 export interface StoreOperationError extends Error {
   name: 'StoreOperationFailed'
@@ -45,7 +75,9 @@ export interface SpaceMetricsItem {
 }
 
 export interface SpaceMetricsStore {
-  incrementTotals: (metricsToUpdate: Record<string, SpaceMetricsItem[]>) => Promise<void>
+  incrementTotals: (
+    metricsToUpdate: Record<string, SpaceMetricsItem[]>
+  ) => Promise<void>
 }
 
 export interface SpaceMetricsCtx {
@@ -62,8 +94,8 @@ export interface InvocationBucket {
   putReceipt: (cid: string, bytes: Uint8Array) => Promise<void>
   putInLink: (cid: string, workflowCid: string) => Promise<void>
   putOutLink: (cid: string, workflowCid: string) => Promise<void>
-  getInLink: (cid: string) => Promise<string|undefined>
-  getWorkflowLink: (cid: string) => Promise<string|undefined>
+  getInLink: (cid: string) => Promise<string | undefined>
+  getWorkflowLink: (cid: string) => Promise<string | undefined>
 }
 
 export interface TaskBucket {
@@ -73,14 +105,14 @@ export interface TaskBucket {
 
 export interface WorkflowBucket {
   put: (Cid: string, bytes: Uint8Array) => Promise<void>
-  get: (Cid: string) => Promise<Uint8Array|undefined>
+  get: (Cid: string) => Promise<Uint8Array | undefined>
 }
 
 export interface DelegationsBucket {
   /** put a delegation into the delegations bucket */
   put: (cid: CID, bytes: ByteView<Delegation>) => Promise<void>
   /** get a delegation from the delegations bucket */
-  get: (cid: CID) => Promise<ByteView<Delegation>|undefined>
+  get: (cid: CID) => Promise<ByteView<Delegation> | undefined>
 }
 
 export interface MetricsTable {
@@ -90,7 +122,6 @@ export interface MetricsTable {
   get: () => Promise<Array<Record<string, any>>>
 }
 
-
 export interface SpaceMetricsTable {
   /**
    * Return the total amount of storage a space has used.
@@ -99,47 +130,51 @@ export interface SpaceMetricsTable {
 }
 
 /**
- * 
+ *
  */
 export interface SubscriptionInput {
   /** DID of the customer who maintains this subscription */
-  customer: DID,
+  customer: DID
   /** DID of the provider who services this subscription */
-  provider: DID,
+  provider: DID
   /** ID of this subscription - should be unique per-provider */
-  subscription: string,
+  subscription: string
   /** CID of the invocation that created this subscription */
   cause: UCANLink
 }
 
 export interface SubscriptionTable {
-  get: (provider: ProviderDID, subscription: string) =>
-    Promise<{ customer: DID } | null>
+  get: (
+    provider: ProviderDID,
+    subscription: string
+  ) => Promise<{ customer: DID } | null>
   /** add a subscription - a relationship between a customer and a provider that will allow for provisioning of consumers */
   add: (consumer: SubscriptionInput) => Promise<{}>
   /** return the count of subscriptions in the system */
   count: () => Promise<bigint>
   /** return a list of the subscriptions a customer has with a provider */
-  findProviderSubscriptionsForCustomer: (customer: DID, provider: DID) =>
-    Promise<Array<{ subscription: string }>>
+  findProviderSubscriptionsForCustomer: (
+    customer: DID,
+    provider: DID
+  ) => Promise<Array<{ subscription: string }>>
 }
 
 export interface ConsumerInput {
   /** the DID of the consumer (eg, a space) for whom services are being provisioned */
-  consumer: DID,
+  consumer: DID
   /** DID of the customer who maintains the subscription for this consumer */
-  customer: DID,
+  customer: DID
   /** the DID of the provider who will provide services for the consumer */
-  provider: DID,
+  provider: DID
   /** the ID of the subscription representing the relationship between the consumer and provider */
-  subscription: string,
+  subscription: string
   /** the CID of the UCAN invocation that created this record */
   cause: UCANLink
 }
 
 export interface ConsumerRecord {
   /** the ID of the subscription representing the relationship between the consumer and provider */
-  subscription: string,
+  subscription: string
   /** the CID of the UCAN invocation that created this record */
   cause: UCANLink
 }
@@ -160,9 +195,15 @@ export interface ConsumerListRecord {
 
 export interface ConsumerTable {
   /** get a consumer record for a given provider */
-  get: (provider: ProviderDID, consumer: DIDKey) => Promise<{ subscription: string } | null>
+  get: (
+    provider: ProviderDID,
+    consumer: DIDKey
+  ) => Promise<{ subscription: string } | null>
   /** get a consumer record for a given subscription */
-  getBySubscription: (provider: ProviderDID, subscription: string) => Promise<{ consumer: DID } | null>
+  getBySubscription: (
+    provider: ProviderDID,
+    subscription: string
+  ) => Promise<{ consumer: DID } | null>
   /** add a consumer - a relationship between a provider, subscription and consumer */
   add: (consumer: ConsumerInput) => Promise<{}>
   /** return the number of consumers */
@@ -172,7 +213,9 @@ export interface ConsumerTable {
   /** return a list of storage providers the given consumer has registered with */
   getStorageProviders: (consumer: DID) => Promise<ProviderDID[]>
   /** List consumers by customer account DID. */
-  listByCustomer: (customer: AccountDID) => Promise<{ results: ConsumerListRecord[] }>
+  listByCustomer: (
+    customer: AccountDID
+  ) => Promise<{ results: ConsumerListRecord[] }>
 }
 
 // TODO: unify this with RecordNotFound in ../billing/tables/lib.js
@@ -190,10 +233,12 @@ export interface Customer {
 // TODO unify this with CustomerStore in ../billing/lib/api.ts
 export interface CustomerTable {
   /** get a customer record */
-  get: (customer: DID<'mailto'>) => Promise<Result<Customer, RecordNotFound<DID<'mailto'>>>>
+  get: (
+    customer: DID<'mailto'>
+  ) => Promise<Result<Customer, RecordNotFound<DID<'mailto'>>>>
 }
 
-export type SpaceService = Pick<Service, "space">
+export type SpaceService = Pick<Service, 'space'>
 
 export type UcanStreamInvocationType = 'workflow' | 'receipt'
 
@@ -257,10 +302,10 @@ declare module 'sst/node/config' {
   export interface SecretResources {
     PRIVATE_KEY: {
       value: string
-    },
+    }
     UCAN_INVOCATION_POST_BASIC_AUTH: {
       value: string
-    },
+    }
     STRIPE_SECRET_KEY: {
       value: string
     }
@@ -279,7 +324,10 @@ type SetPlanFailure = InvalidSubscriptionState | BillingProviderUpdateError
 
 export interface BillingProvider {
   hasCustomer: (customer: AccountDID) => Promise<Result<boolean, Failure>>
-  setPlan: (customer: AccountDID, plan: DID) => Promise<Result<Unit, SetPlanFailure>>
+  setPlan: (
+    customer: AccountDID,
+    plan: DID
+  ) => Promise<Result<Unit, SetPlanFailure>>
 }
 
 export {}
