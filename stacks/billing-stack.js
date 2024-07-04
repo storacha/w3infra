fbd6e6444920c5629df6a432174d6d2af2112e6a
hannahhoward <hannah@hannahhoward.net>
2024-07-03 18:18:11 -0700

    fix(billing): increase parallelization factor

--- stacks/billing-stack.js
+++ stacks/billing-stack.js
@@ -108,7 +108,8 @@ export function BillingStack ({ stack, app }) {
       cdk: {
         eventSource: {
           batchSize: 1,
-          startingPosition: StartingPosition.LATEST
+          startingPosition: StartingPosition.LATEST,
+          parallelizationFactor: 10
         }
       }
     }
