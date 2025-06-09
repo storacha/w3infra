5aa22e6df6d7620dc573adf01d320ac09863338a
Alan Shaw <alan@storacha.network>
2025-06-09 14:31:15 +0100

    fix: storefront service principal

--- test/helpers/fil-client.js
+++ test/helpers/fil-client.js
@@ -2,6 +2,11 @@ import * as Signer from '@ucanto/principal/ed25519'
 import * as DID from '@ipld/dag-ucan/did'
 import { CAR, HTTP } from '@ucanto/transport'
 import { connect } from '@ucanto/client'
+import { getApiEndpoint } from './deployment.js'
+import { mustGetEnv } from '../../lib/env.js'
+
+export const storefrontServiceURL = new URL(getApiEndpoint())
+export const storefrontServicePrincipal = DID.parse(mustGetEnv('UPLOAD_API_DID'))
 
 /**
  * Client connection config for w3filecoin pipeline entrypoint, i.e. API Storefront relies on
@@ -11,16 +16,15 @@ import { connect } from '@ucanto/client'
 export async function getClientConfig (url) {
   // UCAN actors
   const agent = await Signer.generate()
-  const storefrontService = DID.parse('did:web:staging.up.storacha.network')
 
   return {
     invocationConfig: {
       issuer: agent,
       with: agent.did(),
-      audience: storefrontService,
+      audience: storefrontServicePrincipal,
     },
     connection: connect({
-      id: storefrontService,
+      id: storefrontServicePrincipal,
       codec: CAR.outbound,
       channel: HTTP.open({
         url,
