728215ecd692093a07141acdca2b75422a8f0fc8
ash <alan@storacha.network>
2025-11-27 16:33:33 +0000

    feat: support deal tracker service proof (#544)
    This PR adds support for configuring a UCAN proof to be used when
    invoking the deal tracker service.
    
    It is an AWS secret, in order to not exceed env variable limits.
    
    ⚠️ I believe it needs to be set to empty string if it is not used.

--- upload-api/functions/ucan-invocation-router.js
+++ upload-api/functions/ucan-invocation-router.js
@@ -8,6 +8,7 @@ import { base64 } from 'multiformats/bases/base64'
 import * as Sentry from '@sentry/serverless'
 import * as DID from '@ipld/dag-ucan/did'
 import Stripe from 'stripe'
+import * as Proof from '@storacha/client/proof'
 import { Client as IndexingServiceClient } from '@storacha/indexing-service-client'
 import * as UploadAPI from '@storacha/upload-api'
 import * as UCANCaps from '@storacha/capabilities/ucan'
@@ -230,6 +231,7 @@ export async function ucanInvocationRouter(request) {
     PRIVATE_KEY,
     STRIPE_SECRET_KEY,
     INDEXING_SERVICE_PROOF,
+    DEAL_TRACKER_SERVICE_PROOF,
     CONTENT_CLAIMS_PRIVATE_KEY,
     DMAIL_API_KEY,
     DMAIL_API_SECRET,
@@ -360,6 +362,12 @@ export async function ucanInvocationRouter(request) {
     AWS_REGION,
     revocationTableName
   )
+
+  const dealTrackerProofs = []
+  if (DEAL_TRACKER_SERVICE_PROOF && DEAL_TRACKER_SERVICE_PROOF !== 'none') {
+    const proof = await Proof.parse(DEAL_TRACKER_SERVICE_PROOF)
+    dealTrackerProofs.push(proof)
+  }
 
   const dealTrackerConnection = getServiceConnection({
     did: dealTrackerDid,
@@ -577,12 +585,15 @@ export async function ucanInvocationRouter(request) {
     dealTrackerService: {
       connection: dealTrackerConnection,
       invocationConfig: {
-        issuer: getServiceSigner({
-          privateKey: PRIVATE_KEY,
-          did: dealTrackerDid,
-        }),
+        issuer: dealTrackerProofs.length
+          ? serviceSigner
+          : getServiceSigner({
+              privateKey: PRIVATE_KEY,
+              did: dealTrackerDid,
+            }),
         audience: dealTrackerConnection.id,
         with: dealTrackerConnection.id.did(),
+        proofs: dealTrackerProofs
       },
     },
     plansStorage,
--- upload-api/types.ts
+++ upload-api/types.ts
@@ -260,6 +260,9 @@ declare module 'sst/node/config' {
     INDEXING_SERVICE_PROOF: {
       value: string
     },
+    DEAL_TRACKER_SERVICE_PROOF: {
+      value: string
+    },
     /** @deprecated */
     CONTENT_CLAIMS_PRIVATE_KEY: {
       value: string
