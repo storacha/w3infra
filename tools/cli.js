7d58e3764495aa5c58cfb8fd0978a5fe12bc180c
Travis Vachon <travis.vachon@gmail.com>
2025-12-12 17:06:26 -0800

    feat: add a script to update provider field of customer and subscription tables
    when we started using the new `did:web:up.storacha.network` provider, new records in these tables ended up with the new provider, but we never updated the old rows. `customer/get` and possibly other capabilities are currently implemented to make it impossible to retrieve subscriptions that don't use the same provider as the service signer, which means some records are simply not available in the new service.
    
    Updating all of these records to use the current provider will, I think, get our system working as expected.
    
    Additionally, we should have an architectural conversation about what "providers" are doing in our system - I don't think they are functioning as intended and would like to either come to a coherent vision of how they should work or get rid of them.

--- tools/cli.js
+++ tools/cli.js
@@ -8,7 +8,7 @@ import { migrateFromD1ToDynamo } from './d1-migration/add-to-dynamo.js'
 import { printD1ProvisionsEmails } from './d1-migration/print-d1-emails.js'
 import { verifyD1DynamoMigration } from './d1-migration/verify-d1-dynamo-migration.js'
 import { getOldestPiecesPendingDeals } from './get-oldest-pieces-pending-deals.js'
-
+import { updateSubscriptionConsumerProviders }  from './update-subscription-consumer-providers.js'
 dotenv.config({ path: ['.env', '../.env'] })
 
 const cli = sade('w3infra-cli')
@@ -38,5 +38,10 @@ cli
 cli
   .command('verify-d1-migration', 'Verify D1 data has migrated successfully to Dynamo')
   .action(verifyD1DynamoMigration)
+
+cli
+  .command('update-subscription-consumer-providers', 'Update the provider field of subscription and consumer records to did:web:up.storacha.network')
+  .action(updateSubscriptionConsumerProviders)
+  
 
 cli.parse(process.argv)
